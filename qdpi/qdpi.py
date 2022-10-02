4107ac12f39c84832b41740a788a2882b9a6b334
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-10-02 06:47:30 -0400

    multiple backend

--- qdpi/qdpi.py
+++ qdpi/qdpi.py
@@ -4,8 +4,12 @@ from dpdata.driver import Driver
 @Driver.register('qdpi')
 class QDPiDriver(Driver.get_driver("hybrid")):
     """QDPi."""
-    def __init__(self, model: str, charge: int = 0) -> None:
+    def __init__(self, model: str, charge: int = 0, backend="sqm") -> None:
+        if backend == 'sqm':
+            dftb3 = {"type": "sqm", "qm_theory": "dftb3", "charge": charge}
+        elif backend == 'dftb+':
+            dftb3 = {"type": "dftb3", "charge": charge}
         super().__init__([
-            {'type':'dftb3', 'charge': charge},
+            dftb3,
             {'type':'dp', "dp": model},
             ])
