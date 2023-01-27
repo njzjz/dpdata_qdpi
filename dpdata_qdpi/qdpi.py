e3bcf976e7f54a74187386bb35870e7269d48850
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-01-27 06:26:59 -0500

    run pre-commit

--- dpdata_qdpi/qdpi.py
+++ dpdata_qdpi/qdpi.py
@@ -1,15 +1,18 @@
 from dpdata.driver import Driver
 
 
-@Driver.register('qdpi')
+@Driver.register("qdpi")
 class QDPiDriver(Driver.get_driver("hybrid")):
     """QDPi."""
+
     def __init__(self, model: str, charge: int = 0, backend="sqm") -> None:
-        if backend == 'sqm':
+        if backend == "sqm":
             dftb3 = {"type": "sqm", "qm_theory": "dftb3", "charge": charge}
-        elif backend == 'dftb+':
+        elif backend == "dftb+":
             dftb3 = {"type": "dftb3", "charge": charge}
-        super().__init__([
-            dftb3,
-            {'type':'dp', "dp": model},
-            ])
+        super().__init__(
+            [
+                dftb3,
+                {"type": "dp", "dp": model},
+            ]
+        )
