1381b98504a277cbe78c5ebf9b3972a7d0a89b03
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-01-27 21:45:32 -0500

    use and run pre-commit to format codes (#7)
    - add pre-commit config
    - run pre-commit

--- tests/test_qdp.py
+++ tests/test_qdp.py
@@ -5,11 +5,13 @@ import pytest
 from dpdata_qdpi import QDPiDriver
 from dpdata.plugins.ase import ASEMinimizer
 
+
 @pytest.fixture
 def ch4():
     this_dir = Path(__file__).parent
     ch4 = dpdata.System(str(this_dir / "ch4.xyz"))
     return ch4
+
 
 @pytest.fixture
 def qdpi():
@@ -20,12 +22,14 @@ def qdpi():
     )
     return qdpi
 
+
 @pytest.mark.skip
 def test_single_point(ch4, qdpi):
     # single point calculation
     p = ch4.predict(driver=qdpi)
-    print("Energies:", p['energies'][0])
-    print("Forces:", p['forces'][0])
+    print("Energies:", p["energies"][0])
+    print("Forces:", p["forces"][0])
+
 
 @pytest.mark.skip
 def test_optimization(ch4, qdpi):
@@ -34,6 +38,6 @@ def test_optimization(ch4, qdpi):
         driver=qdpi,
     )
     p = ch4.minimize(minimizer=lbfgs)
-    print("Coordinates:", p['coords'][0])
-    print("Energies:", p['energies'][0])
-    print("Forces:", p['forces'][0])
+    print("Coordinates:", p["coords"][0])
+    print("Energies:", p["energies"][0])
+    print("Forces:", p["forces"][0])
