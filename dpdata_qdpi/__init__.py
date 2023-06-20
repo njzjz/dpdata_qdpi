baa7ed235d216c3fb4acb16b0cf65487c791f51b
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-06-20 02:39:35 -0400

    Update __init__.py

--- dpdata_qdpi/__init__.py
+++ dpdata_qdpi/__init__.py
@@ -1,2 +1,7 @@
 # prevent circular import
 """dpdata plugin for QDPI."""
+import dpdata  # noqa:F401
+from .dftb3 import DFTB3Driver
+from .qdpi import QDPiDriver
+
+__all__ = ['DFTB3Driver', 'QDPiDriver']
