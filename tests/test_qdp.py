41f75f569d8de9c7121bbef7d869a56b6a62362c
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-04-03 04:26:22 -0400

    skip dftb+ tests

--- tests/test_qdp.py
+++ tests/test_qdp.py
@@ -17,7 +17,7 @@ def ch4():
 @pytest.fixture(
     params=[
         "sqm",
-        "dftb+",
+        pytest.param("dftb+", marks=pytest.mark.skip(reason="TODO: wait for https://gitlab.com/ase/ase/-/merge_requests/3282")),
         "dftb+api",
     ]
 )
